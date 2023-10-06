f4c5b474322773d5c4f0ffe6591217e75eac5ed5
lucas.pastre <lucasfpastre@gmail.com>
2023-10-06 11:28:14 -0300

    Commit do projeto finalizado

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gameType: string = "Digital PS4"
+  @Input()
+  gamePrice: string = "R$ 59,99"
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  gameCover: string = ""
+  @Input()
+  labelData: string = "";
+  @Input()
+  gameType: string = "PS5 | PS4"
+  @Input()
+  gamePrice: string = "R$ 59,99"
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
